b8ce7a8e226c4f0c275c98ee65494dd4bcab514d
icbaladitya <icbaladityafirmansyah@gmail.com>
2024-12-12 07:35:29 +0700

    v.1.0.0

--- src/main/java/com/nutech/hometest/config/SecurityConfig.java
+++ src/main/java/com/nutech/hometest/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.nutech.hometest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,7 +24,7 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/login", "/registration", "/banner", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
+                .requestMatchers(HttpMethod.OPTIONS, "/login", "/registration", "/banner", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
